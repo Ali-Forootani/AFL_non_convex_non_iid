ec38d2145adc5a3332a435b8a282e15edcc1c8d6
Ali Forootani <forootani@mpi-magdeburg.mpg.de>
2024-12-28 23:58:23 +0100

    bash file

--- src/fdl_async1.sh
+++ src/fdl_async1.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
-#SBATCH --job-name=my_fdl_async_job
+#SBATCH --job-name=my_federated_rl
 #SBATCH --time=0-40:30:00
 #SBATCH --gres=gpu:nvidia-a100:1
-#SBATCH --mem-per-cpu=256G
-#SBATCH --constraint a100-vram-80G
-#SBATCH --output="/data/bio-eng-llm/fed_learning_wind_proj/logs/%x-%j.log"
+#SBATCH --mem-per-cpu=32G
+#SBATCH --output="/data/bio-eng-llm/AFL_non_convex/logs/%x-%j.log"
 
 # Change directory within the script
+
+
+# Load any necessary modules, if require
+
 source /data/bio-eng-llm/virtual_envs/dnn_env/bin/activate
 
 # Execute the Python script
-python /data/bio-eng-llm/fed_learning_wind_proj/src/training_wind_psr_lstm_fl_async.py
+python /data/bio-eng-llm/AFL_non_convex/training_mnist_fl_async_non_convex_random_selection.py
 
